5e54e8d205c801148530bfca896dd57bd9d6dd3a
RumyantsevMichael <Rumyantsev.Michael@gmail.com>
2018-02-25 14:03:58 +0300

    Changed build

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,17 @@ import typescript from 'rollup-plugin-typescript2';
 
 const config = {
     input: 'src/index.ts',
-    output: {
-        name: 'chansole',
-        file: 'index.js',
-        format: 'cjs'
-    },
+    output: [
+        {
+            file: 'index.esm.js',
+            format: 'es'
+        },
+        {
+            name: 'chansole',
+            file: 'index.umd.js',
+            format: 'umd'
+        }
+    ],
     plugins: [
         typescript({
             tsconfig: 'tsconfig.json'
